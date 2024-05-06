7f1903f12dedb3358455f0f43b928311993f213f
Is2095 <ismael2095951@gmail.com>
2024-05-06 14:39:41 -0300

    colocando validaciones al formulario y response

--- formulario/js/script.js
+++ formulario/js/script.js
@@ -7,7 +7,6 @@ function provincias() {
     fetch("https://apis.datos.gob.ar/georef/api/provincias")
     .then(res => res.ok ? res.json() : Promise.reject(res))
     .then(res => {
-        console.log(res.provincias, 'respuesta');
         let $opciones = `<option value="Elige una Provincia">Elige una Provincia</option>`;
         res?.provincias?.forEach(element => $opciones += `<option value="${element.nombre}">${element.nombre}</option>`);
         $provincia.innerHTML = $opciones;
