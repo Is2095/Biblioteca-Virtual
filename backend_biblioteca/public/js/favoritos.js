8866deae535a644760e9edf3c080d7a6b1e50b89
Is2095 <ismael2095951@gmail.com>
2024-06-21 21:57:01 -0300

    mejora en paso de info del libro para enviarlo a la BD

--- backend_biblioteca/public/js/favoritos.js
+++ backend_biblioteca/public/js/favoritos.js
@@ -1,45 +1,29 @@
 
-function toggleFavorite(button) {
+function toggleFavorite(button, dato) {
 
     const corazonRojo = button.classList.toggle('active');
-    const id = button.getAttribute("data-id")
-    const authors = button.getAttribute("data-authors")
-    const description = button.getAttribute("data-description")
-    const imageLink = button.getAttribute("data-imageLink")
-    const pageCount = button.getAttribute("data-pageCount")
-    const published_date = button.getAttribute("data-published_date")
-    const title = button.getAttribute("data-title")
-    const language = button.getAttribute("data-language")
-    const id_usuario = button.getAttribute("data-id_usuario")
-    const categoria = button.getAttribute("data-categoria")
-    console.log(categoria, 'categoria funcion favorito');
-
-    const datosLibroFavorito = { id, authors, description, imageLink, pageCount, published_date, title, language, id_usuario, categoria}
-
+   
     if (!corazonRojo) {
         fetch(`http://localhost:3001/api`, { 
             method: 'DELETE' ,
             headers: {
                 'Content-Type': 'application/json' 
             },
-            body: JSON.stringify({id, id_usuario})
+            body: JSON.stringify({id: dato.id, id_usuario: dato.id_usuario})
         })
             .then(res => res.json())
             .then(res => console.log(res))
-            .catch(err => console.log(err))
+            .catch(err => console.log(err));
     } else {
         fetch(`http://localhost:3001/api/favoritos`, {
              method: 'POST',
              headers: {
                 'Content-Type': 'application/json'
              },
-             body: JSON.stringify(datosLibroFavorito)
+             body: JSON.stringify(dato)
             })
             .then(res => res.json())
             .then(res => console.log(res))
-            .catch(err => console.log(err))
-    }
-
-
-
-}
+            .catch(err => console.log(err));
+    };
+};
--- backend_biblioteca/public/js/index.js
+++ backend_biblioteca/public/js/index.js
@@ -5,74 +5,60 @@ const salir = $d.getElementById('hrefSalir');
 const registrarse = $d.getElementById('hrefRegistrarse');
 const botonFavorito = $d.getElementById('botonFavorito');
 
-const dato = sessionStorage.getItem('idUsuario');
+const datoUsuario = sessionStorage.getItem('idUsuario');
 
-if (dato) {
-    entrar.style.opacity = '0'
-    salir.style.opacity = '1'
-    registrarse.style.opacity = '0'
-    
-    
+if (datoUsuario) {
+    entrar.style.opacity = '0';
+    salir.style.opacity = '1';
+    registrarse.style.opacity = '0';
 } else {
-    entrar.style.opacity = '1'
-    salir.style.opacity = '0'
-    registrarse.style.opacity = '1'
+    entrar.style.opacity = '1';
+    salir.style.opacity = '0';
+    registrarse.style.opacity = '1';
 }
-
 
 const llamar = async (categoria) => {
 
-    const contenedor = document.getElementById('librosBD')
+    const contenedor = document.getElementById('librosBD');
     contenedor.innerHTML = '';
 
-    const id_usuario = sessionStorage.getItem('idUsuario')
+    const id_usuario = sessionStorage.getItem('idUsuario');
 
-    const response = await fetch(`http://localhost:3001/api/favoritos/${parseInt(id_usuario)}`)
-
-    const arrayFavoritos = await response.json()
+    const response = await fetch(`http://localhost:3001/api/favoritos/${parseInt(id_usuario)}`);
+    const arrayFavoritos = await response.json();
 
     const insertarDatos = (datos) => {
-        console.log(datos, 'datos');
-
             datos.forEach((objeto, index) => {
-                const div = document.createElement('div')
-                div.classList.add('carousel-item')
+                const div = document.createElement('div');
+                div.classList.add('carousel-item');
                 if (index === 0) {
-                    div.classList.add('active')
-                }
-                const { id, authors, description, imageLink, language, pageCount, title, published_date, id_libro } = objeto;
-                const datosLibroFavorito = { id, authors, description, language, pageCount, title, published_date, id_libro }
-                const esFavorito = arrayFavoritos.some(elemt => elemt.id === id)
-                console.log(esFavorito, 'esFavorito', arrayFavoritos, id);
+                    div.classList.add('active');
+                };
+
+                const { id, authors, description, imageLink, language, pageCount, title, publishedDate, id_libro } = objeto;
+                const datosLibroFavorito = { id, authors, description, language, pageCount, title, publishedDate, id_usuario, categoria };
+                const esFavorito = arrayFavoritos.some(elemt => elemt.id === id);
 
                 if (authors && description && imageLink && language && title) {
                     div.innerHTML = `
-                            <button id="botonCorazon" 
-                            data-id="${id}" 
-                            data-authors="${authors}" 
-                            data-description="${description}" 
-                            data-imageLink="${imageLink}" 
-                            data-language="${language}" 
-                            data-pageCount="${pageCount}" 
-                            data-title="${title}" 
-                            data-published_date="${published_date}" 
-                            data-id_usuario="${id_usuario}"
-                            data-categoria="${categoria}"
-                            class="botonCorazon ${esFavorito ? "active" : ""} ${dato ? "" : "noHayDatos"}" onClick="toggleFavorite(this)"><i class="bi bi-heart-fill"></i></button>
+                            <button id="botonCorazon" class="botonCorazon ${esFavorito ? "active" : ""} ${datoUsuario ? "" : "noHayDatos"}">
+                                <i class="bi bi-heart-fill"></i>
+                            </button>
                             <img class="imagenCarrusel" src="${imageLink}"></img>
                             <p>título ${title}</p>
                             <p>Autor ${authors}</p>
-                            <p>fecha de impreso: ${published_date}</p>
+                            <p>fecha de impreso: ${publishedDate}</p>
                             <p>cantidad de hojas: ${pageCount}</p>
                     `;
+                    const boton = div.querySelector('#botonCorazon');
+                    boton.addEventListener('click', () => toggleFavorite(boton, datosLibroFavorito))
                     contenedor.appendChild(div);
                 };
             });
-
     };
 
     fetch(`http://localhost:3001/api/libros?categoria=${categoria}`)
         .then(res => res.json())
         .then(res => insertarDatos(res))
         .catch(err => console.log(err));
-}   
+};  
